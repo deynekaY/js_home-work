629951eaed9ac7d511248b389686942bbeb01753
deynekaY <e.deyneka@imagecms.net>
2022-07-20 13:48:20 +0300

    codeWars

---
+++ script.js
@@ -0,0 +1,28 @@
+function makeNegative(num) {
+    if (num > 0) {
+        let mult = num * -1;
+        return mult;
+    } else {
+        return num;
+    }
+}
+
+
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    let total = laLigaGoals + copaDelReyGoals + championsLeagueGoals // code goes here
+    return total;
+}
+
+
+
+
+
+
+function lovefunc(flower1, flower2) {
+    if (flower1 > 0 != flower2 < 0) {
+        return true
+    } else {
+        return false
+    }
+}
+
